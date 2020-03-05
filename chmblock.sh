36ca8aebfb7a0f9ed75e68a8a3a1034d8069c75f
Alexey Kostin <kostin@users.noreply.github.com>
2020-03-05 12:51:06 +0300

    Update chmblock.sh

--- chmblock.sh
+++ chmblock.sh
@@ -4,13 +4,31 @@ DIR="$1"
 
 if [ ! -d "${DIR}" ]
 then
-    	echo "Site dir doesnt exists!"
-        exit 0
+ echo "Site dir doesnt exists!"
+ exit 0
 fi
 
 
-find ${DIR} -type f -print0 | xargs -0 chmod 644
-find ${DIR} -type d -print0 | xargs -0 chmod 755
-find ${DIR}/sites/default/files/ -type d -print0 | xargs -0 chmod 777
-find ${DIR}/sites/default/files/ -type f -print0 | xargs -0 chmod 666
-chown -R root ${DIR}/*
+if [ -d "${DIR}/sites/default/files/" ]
+then
+  echo "Detected Drupal site!"
+  find ${DIR} -type f -print0 | xargs -0 chmod 644
+  find ${DIR} -type d -print0 | xargs -0 chmod 755
+  find ${DIR}/sites/default/files/ -type d -print0 | xargs -0 chmod 777
+  find ${DIR}/sites/default/files/ -type f -print0 | xargs -0 chmod 666
+  chown -R root ${DIR}/*
+  exit 0
+fi
+
+if [ -d "${DIR}/wp-content/uploads/" ]
+then
+  echo "Detected Wordpress site!"
+  find ${DIR} -type f -print0 | xargs -0 chmod 644
+  find ${DIR} -type d -print0 | xargs -0 chmod 755
+  find ${DIR}/wp-content/uploads/ -type d -print0 | xargs -0 chmod 777
+  find ${DIR}/wp-content/uploads/ -type f -print0 | xargs -0 chmod 666
+  chown -R root ${DIR}/*
+  exit 0
+fi
+
+echo "Cant detect CMS and change permission automatically!"
