edda32e5e090ee7cb174a712973bce622f4300a8
Alexey Kostin <kostin@users.noreply.github.com>
2020-03-18 20:11:27 +0300

    Update chmblock.sh

--- chmblock.sh
+++ chmblock.sh
@@ -12,21 +12,21 @@ fi
 if [ -d "${DIR}/sites/default/files/" ]
 then
   echo "Detected Drupal site!"
-  find ${DIR} -type f -print0 | xargs -0 chmod 644
-  find ${DIR} -type d -print0 | xargs -0 chmod 755
-  find ${DIR}/sites/default/files/ -type d -print0 | xargs -0 chmod 777
-  find ${DIR}/sites/default/files/ -type f -print0 | xargs -0 chmod 666
-  chown -R root ${DIR}/*
+  find ${DIR} -type f -exec chmod 644 {} \;
+  find ${DIR} -type d -exec chmod 755 {} \;
+  find ${DIR}/sites/default/files/ -type d -exec chmod 777 {} \;
+  find ${DIR}/sites/default/files/ -type f -exec chmod 666 {} \;
+  chown -R root:root ${DIR}/*
   exit 0
 fi
 
 if [ -d "${DIR}/wp-content/uploads/" ]
 then
   echo "Detected Wordpress site!"
-  find ${DIR} -type f -print0 | xargs -0 chmod 644
-  find ${DIR} -type d -print0 | xargs -0 chmod 755
-  find ${DIR}/wp-content/uploads/ -type d -print0 | xargs -0 chmod 777
-  find ${DIR}/wp-content/uploads/ -type f -print0 | xargs -0 chmod 666
+  find ${DIR} -type f -exec chmod 644 {} \;
+  find ${DIR} -type d -exec chmod 755 {} \;
+  find ${DIR}/wp-content/uploads/ -type d -exec chmod 777 {} \;
+  find ${DIR}/wp-content/uploads/ -type f -exec chmod 666 {} \;
   chown -R root ${DIR}/*
   exit 0
 fi
